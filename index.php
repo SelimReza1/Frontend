d324922525d0382ae512be51204a847302659dd5
Selim Reza <selimreza9920@gmail.com>
2018-08-09 00:44:11 +0600

    menubar created

---
+++ index.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>psd to html part1</title>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700" rel="stylesheet">
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+
+<header>
+    <div class="header-area">
+        <div class="container">
+            <div class="logo">
+            <a href="index.php"><img src="img/diu.png" alt="diu image" /></a>
+        </div>
+            <div class="header-right">
+                <div class="search-icon">
+                    <i class="fas fa-search"></i>
+                </div>
+            <div class="main-menu">
+                <ul>
+                    <li><a href="#">Home</a> </li>
+                    <li><a href="#">Features</a> </li>
+                    <li><a href="#">Pages</a>
+                    <ul class="submenu">
+                        <li><a href="#">About us</a></li>
+                        <li><a href="#">Services</a>
+                            <ul class="submenu">
+                                <li><a href="#">About us</a></li>
+                                <li><a href="#">Services</a></li>
+                                <li><a href="#">Shop</a></li>
+                                <li><a href="#">404 pages</a></li>
+                                <li><a href="#">Coming soon</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">404 pages</a></li>
+                        <li><a href="#">Coming soon</a></li>
+                    </ul>
+                    </li>
+                    <li><a href="#">Portfolio</a> </li>
+                    <li><a href="#">Blog</a> </li>
+                    <li><a href="#">Contact Us</a> </li>
+                </ul>
+            </div>
+        </div>
+        </div>
+    </div>
+</header>
+
+</body>
+</html>
